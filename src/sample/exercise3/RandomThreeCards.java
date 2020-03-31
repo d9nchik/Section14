1df3ccee15999a563e0bed1341d7adc32493f741
Danylo Halaiko <dan.galaiko@gmail.com>
2020-03-31 13:48:44 +0300

    Generator of three different card has been done.

---
+++ src/sample/exercise3/RandomThreeCards.java
@@ -0,0 +1,35 @@
+package sample.exercise3;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RandomThreeCards extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        HBox pane = new HBox();
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 1; i <= 54; i++)
+            arrayList.add(i);
+        Collections.shuffle(arrayList);
+
+        for (int i = 0; i < 3; i++)
+            pane.getChildren().add(new ImageView("src/image/card/" + arrayList.get(i) + ".png"));
+
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Exercise 3"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show();
+    }
+}
